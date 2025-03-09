b24f5e1628322375704a115788c87cddc3680eca
SerhiiMyronets <hardmail88@gmail.com>
2025-03-08 18:46:40 -0800

    init

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const db = require("./db");
+const apiRouter = require("./routes");
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/api', apiRouter)
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World!!!");
+})
+
+
+db.authenticate().then(() => {
+    console.log("Connected to the database!!");
+    return db.sync()
+}).then(() => {
+    console.log("Database synchronized!");
+    app.listen(port, () => {
+        console.log("Listening on port " + port);
+    })
+})
+.catch((err) => {
+    console.error('Unable to connect to DB');
+    console.log(err);
+})
---
+++ src/models.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("./db");
+
+const KV = sequelize.define("KV", {
+    key: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    value: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+})
+
+module.exports = { KV };
